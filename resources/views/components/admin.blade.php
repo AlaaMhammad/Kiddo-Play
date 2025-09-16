2179b8a95bec2b16d7bad554fbcee0b903279afa
AlaaMhammad <alaamhammad2003@gmail.com>
2025-09-16 16:14:49 +0300

    update logo-icon  commit

--- resources/views/components/admin.blade.php
+++ resources/views/components/admin.blade.php
@@ -13,7 +13,7 @@
     <meta name="description" content="" />
 
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{ asset('dashboard/assets/img/favicon/favicon.ico') }}" />
+    <link rel="icon" type="image/x-icon" href="{{ asset('dashboard/assets/img/kiddo.png') }}" />
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
