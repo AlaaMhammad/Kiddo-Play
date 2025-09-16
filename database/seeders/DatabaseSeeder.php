13156d6b1f0ccebb4bc3ce70e6c7cd25d35641a2
AlaaMhammad <alaamhammad2003@gmail.com>
2025-09-16 15:27:33 +0300

    update commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,8 @@ class DatabaseSeeder extends Seeder
 
         // $this->call(UserSeeder::class);
         $this->call([
-            UserSeeder::class,
             RoleSeeder::class,
+            UserSeeder::class,
         ]);
 
         // User::factory()->create([
