fa2fdf74f28c00eb87a565830d72a07d777365b1
AlaaMhammad <alaamhammad2003@gmail.com>
2025-12-18 12:40:49 +0200

    edit DailyGoals  views

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -21,7 +21,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // Job لإنشاء Daily Goals عند منتصف الليل
-        $schedule->job(new GenerateDailyGoalsJob)->dailyAt('00:00');
+        $schedule->job(new GenerateDailyGoalsJob)->daily();
 
         // مثال: يمكنك إضافة أوامر أخرى
         // $schedule->command('some:command')->hourly();
